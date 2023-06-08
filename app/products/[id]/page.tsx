bd76432a5806282c6b44f357db6d7157927c6dba
Vakho <vakho.daraselia@gmail.com>
2023-06-08 14:31:29 +0400

    resolve materialUi problems, no need for wrappers

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Metadata } from "next";
-
-import Link from "next/link";
 import { getSingleProduct } from "@/libs/getSingleProducts";
-import { getProducts } from "@/libs/getAllProducts";
+import Image from "next/image";
+import {
+  AppBar,
+  Typography,
+  Card,
+  Grid,
+  Button,
+} from "@/components/muiComponents/Mui";
 
 type Params = {
   params: {
@@ -25,13 +30,69 @@ async function ProductPage({ params: { id } }: Params) {
   const data = await getSingleProduct(id);
 
   return (
-    <div>
-      ProductPage - {id}
+    <Grid
+      container
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
       {data.map((item) => (
-        <h3 key={item.id}>{item.id}</h3>
+        <Grid
+          container
+          key={item.id}
+          display="flex"
+          justifyContent="space-evenly"
+          alignItems="center"
+          direction="column"
+          width="100%"
+          height="100%"
+          spacing={5}
+        >
+          <Grid item alignSelf="flex-start">
+            <Typography variant="h4" marginLeft="200px">
+              {item.title}
+            </Typography>
+          </Grid>
+          <Grid item alignSelf="flex-start">
+            <Image
+              src={item.image}
+              alt="product"
+              width={250}
+              height={250}
+              style={{
+                marginLeft: "200px",
+                boxShadow: "5px 5px 10px rgba(0,0,0, 0.5)",
+                padding: "10px",
+                borderRadius: "10px",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Card sx={{ width: "300px", height: "300px", padding: "20px" }}>
+              <Typography variant="h5">
+                Price: <strong>{item.price}$</strong>
+              </Typography>
+              <Button
+                disabled={false}
+                variant="contained"
+                sx={{
+                  display: "block",
+                  marginInline: "auto",
+                  marginTop: "230px",
+                  backgroundColor: "#82b541",
+                  width: "250px",
+                }}
+              >
+                Add to cart
+                <AppBar>Appbar</AppBar>
+              </Button>
+            </Card>
+          </Grid>
+        </Grid>
       ))}
-      <Link href="/">link to home</Link>
-    </div>
+      {/* <Link href="/">link to home</Link> */}
+    </Grid>
   );
 }
 
---
+++ components/muiComponents/Mui.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+  AppBar,
+  Accordion,
+  Alert,
+  Avatar,
+  Backdrop,
+  Badge,
+  Button,
+  Card,
+  Container,
+  Grid,
+  Modal,
+  CircularProgress,
+  Rating,
+  Select,
+  Skeleton,
+  TextField,
+  Tooltip,
+  Typography,
+  Box
+} from "@mui/material";
+
+export {
+  AppBar,
+  Accordion,
+  Alert,
+  Avatar,
+  Backdrop,
+  Badge,
+  Button,
+  Card,
+  Container,
+  Grid,
+  Modal,
+  CircularProgress,
+  Rating,
+  Select,
+  Skeleton,
+  TextField,
+  Tooltip,
+  Typography,
+  Box
+};
